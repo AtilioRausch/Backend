bca80dfb6a27966ea6976c9a0b9c4f020e1cf2b0
Atilio O. Rausch <atilio.rausch@uner.edu.ar>
2023-10-30 19:28:26 -0300

    Add files via upload

---
+++ Desafio5/src/dao/mongoDB/cartManagerMongo.js
@@ -0,0 +1,35 @@
+import { cartsModel } from "../../db/models/carts.model.js";
+
+/* import { prodmanager } from "./productManagerMongo.js"; */
+
+class CartsManager {
+    async createCart() {
+        const result = await cartsModel.create({products: []});
+        return result;
+    }
+
+
+    async getCartProducts(cid) {
+        const result = await cartsModel.findById(cid);
+        return result;                    
+    }
+
+
+    async addProductToCart(cid, pid){
+        const selectedCart = await cartsModel.findById(cid);        
+        const productIndex = selectedCart.products.findIndex(p => p.product.equals(pid));
+        if (productIndex !== -1) {
+            selectedCart.products[productIndex].quantity += 1;
+        } else {
+            selectedCart.products.push({ 
+                product: pid, 
+                quantity: 1 
+            });
+        }
+        /* await cartsModel.updateOne({ _id: cid }, {products: selectedCart.products}); */
+        await selectedCart.save()
+        return selectedCart;
+    }    
+}
+
+export const cManager = new CartsManager();
